7fb3c8719a181aaadcf86bcab5628bdd5b774e54
ladynamic <veronicalarroy@gmail.com>
2025-09-16 06:30:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 # Write directly to the app
 st.title(f" :cup_with_straw: Customize your smoothie! :cup_with_straw:")
